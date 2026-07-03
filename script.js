ccbea642c32d356969fa4a00e680194753046947
Fotio-SynkCORE <preciousfotio@gmail.com>
2026-07-03 17:16:10 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-console.log("Welcome to PreCode Portfolio!");
+console.log("Welcome to Fotio Precious' Portfolio!");
+
+// Mobile Menu
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+});
+
+// Dark Mode
+const themeToggle = document.getElementById("theme-toggle");
+
+if(themeToggle){
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+
+        const icon = themeToggle.querySelector("i");
+
+        if(document.body.classList.contains("light-mode")){
+            icon.classList.remove("fa-moon");
+            icon.classList.add("fa-sun");
+        }else{
+            icon.classList.remove("fa-sun");
+            icon.classList.add("fa-moon");
+        }
+    });
+}
 
 const slides = document.querySelectorAll(".slide");
 
